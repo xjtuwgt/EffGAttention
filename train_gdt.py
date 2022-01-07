bd41f370e6630cb64f71109474850b3dc82d81cc
王广涛 <guangtao.wang@jd.com>
2022-01-07 08:32:51 -0800

    first commit

--- train_gdt.py
+++ train_gdt.py
@@ -44,7 +44,7 @@ def model_train(g, model, features, labels, train_mask, val_mask, test_mask, opt
             t0 = time.time()
         # forward
         logits = model(g, features)
-        train_mask = label_mask_drop(train_mask=train_mask_backup, drop_ratio=0.1)
+        # train_mask = label_mask_drop(train_mask=train_mask_backup, drop_ratio=0.1)
         loss = loss_fcn(logits[train_mask], labels[train_mask])
         optimizer.zero_grad()
         loss.backward()
