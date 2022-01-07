c8561946735d8894eb1d113d5165e84fcd8285fc
王广涛 <guangtao.wang@jd.com>
2022-01-07 10:40:44 -0800

    first commit

--- codes/default_argparser.py
+++ codes/default_argparser.py
@@ -96,7 +96,7 @@ def default_parser():
     parser.add_argument('--alpha', type=float, default=0.1)
     parser.add_argument('--hidden_dim', type=int, default=64)
     parser.add_argument('--head_num', type=int, default=8)
-    parser.add_argument('--layers', type=int, default=32)
+    parser.add_argument('--layers', type=int, default=2)
     parser.add_argument('--negative_slope', type=float, default=0.2)
     parser.add_argument('--top_p', type=float, default=0.75)
     parser.add_argument('--top_k', type=int, default=8)
