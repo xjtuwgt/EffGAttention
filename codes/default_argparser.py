49bf8591a0d50b133570d548655dbdb98f8945ee
王广涛 <guangtao.wang@jd.com>
2022-01-08 11:56:22 -0800

    first commit

--- codes/default_argparser.py
+++ codes/default_argparser.py
@@ -85,7 +85,7 @@ def default_parser():
     parser.add_argument('--node_emb_dim', type=int, default=300)
     parser.add_argument('--num_classes', type=int, default=-1)
     # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    parser.add_argument('--feat_drop', type=float, default=0.3)
+    parser.add_argument('--feat_drop', type=float, default=0.25)
     parser.add_argument('--attn_drop', type=float, default=0.25)
     parser.add_argument('--residual', type=boolean_string, default='true')
     parser.add_argument('--ppr_diff', type=boolean_string, default='true')
@@ -93,7 +93,7 @@ def default_parser():
     # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     parser.add_argument('--central_emb', type=boolean_string, default='true')
     parser.add_argument('--max_degree', type=int, default=200)
-    parser.add_argument('--degree_emb_dim', type=int, default=768)
+    parser.add_argument('--degree_emb_dim', type=int, default=512)
     parser.add_argument('--gnn_hop_num', type=int, default=6)
     parser.add_argument('--alpha', type=float, default=0.1)
     parser.add_argument('--hidden_dim', type=int, default=64)
