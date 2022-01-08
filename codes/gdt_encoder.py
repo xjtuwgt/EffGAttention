c69b4bf761d8b55a57abeff3dc6f401569b45571
王广涛 <guangtao.wang@jd.com>
2022-01-08 11:23:22 -0800

    first commit

--- codes/gdt_encoder.py
+++ codes/gdt_encoder.py
@@ -11,9 +11,11 @@ class GDTEncoder(nn.Module):
         super(GDTEncoder, self).__init__()
         self.config = config
         if self.config.central_emb:
-            self.central_emb_layer = EmbeddingLayer(num=self.config.max_degree, dim=self.config.degree_emb_dim)
-            self.feature_map = nn.Linear(in_features=self.config.node_emb_dim, out_features=self.config.degree_emb_dim)
-            input_dim = self.config.degree_emb_dim
+            self.central_emb_layer = EmbeddingLayer(num=self.config.max_degree,
+                                                    dim=self.config.degree_emb_dim,
+                                                    project_dim=self.config.node_emb_dim)
+            self.feature_map = nn.Linear(in_features=self.config.node_emb_dim, out_features=self.config.node_emb_dim)
+            input_dim = self.config.node_emb_dim
         else:
             input_dim = self.config.node_emb_dim
             self.central_emb_layer = None
