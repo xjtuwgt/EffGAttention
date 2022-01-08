c11781695ef15f1deaa5a865ab9e39c2998a3370
王广涛 <guangtao.wang@jd.com>
2022-01-08 11:44:44 -0800

    first commit

--- codes/gdt_encoder.py
+++ codes/gdt_encoder.py
@@ -56,7 +56,7 @@ class GDTEncoder(nn.Module):
     def reset_parameters(self):
         gain = nn.init.calculate_gain('relu')
         nn.init.xavier_normal_(self.classifier.weight, gain=gain)
-        gain = small_init_gain_v2(d_in=self.config.node_emb_dim, d_out=self.config.degree_emb_dim)
+        gain = small_init_gain_v2(d_in=self.config.degree_emb_dim, d_out=self.config.degree_emb_dim)
         nn.init.xavier_normal_(self.feature_map.weight, gain=gain)
 
     def forward(self, graph, inputs: Tensor):
