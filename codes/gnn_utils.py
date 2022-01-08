56d6d381848221a20c814958b6b0a4305753af77
王广涛 <guangtao.wang@jd.com>
2022-01-08 12:18:55 -0800

    first commit

--- codes/gnn_utils.py
+++ codes/gnn_utils.py
@@ -204,7 +204,7 @@ class EmbeddingLayer(nn.Module):
     def init(self):
         """Initializing the embeddings.
         """
-        gain = 1.0 / math.sqrt(self.dim)
+        gain = small_init_gain(d_in=self.dim, d_out=self.dim)
         INIT.xavier_normal_(self.embedding.weight, gain)
         if isinstance(self.projection, nn.Linear):
             INIT.xavier_normal_(self.projection.weight, gain=gain)
