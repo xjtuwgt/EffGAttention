43f425fd133efc17642408a1bb20070cc5242aba
王广涛 <guangtao.wang@jd.com>
2022-01-08 22:39:26 -0800

    first commit

--- codes/gdt_layers.py
+++ codes/gdt_layers.py
@@ -7,7 +7,7 @@ from dgl.nn.functional import edge_softmax
 from torch.nn import LayerNorm as layerNorm
 from dgl.base import DGLError
 from dgl.utils import expand_as_pair
-from codes.gnn_utils import PositionwiseFeedForward, small_init_gain, small_init_gain_v2
+from codes.gnn_utils import PositionwiseFeedForward, small_init_gain
 from codes.gnn_utils import top_kp_attention, top_kp_attn_normalization
 
 
@@ -64,7 +64,7 @@ class GDTLayer(nn.Module):
         self.reset_parameters()
 
     def reset_parameters(self):
-        gain = small_init_gain_v2(d_in=self._in_ent_feats, d_out=self._out_feats)
+        gain = small_init_gain(d_in=self._in_ent_feats, d_out=self._out_feats)
         nn.init.xavier_normal_(self.fc_head.weight, gain=gain)
         nn.init.xavier_normal_(self.fc_tail.weight, gain=gain)
         nn.init.xavier_normal_(self.fc_ent.weight, gain=gain)
