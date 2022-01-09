4a9a6f49f6ae8de73e66a25a2f8646d5acfe61f7
王广涛 <guangtao.wang@jd.com>
2022-01-08 18:21:39 -0800

    first commit

--- codes/gdt_layers.py
+++ codes/gdt_layers.py
@@ -64,10 +64,7 @@ class GDTLayer(nn.Module):
         self.reset_parameters()
 
     def reset_parameters(self):
-        if self.layer_num <= 3:
-            gain = nn.init.calculate_gain('relu')
-        else:
-            gain = small_init_gain(d_in=self._in_ent_feats, d_out=self._out_feats)/math.sqrt(self.layer_num)
+        gain = small_init_gain(d_in=self._in_ent_feats, d_out=self._out_feats)/math.sqrt(self.layer_num)
         nn.init.xavier_normal_(self.fc_head.weight, gain=gain)
         nn.init.xavier_normal_(self.fc_tail.weight, gain=gain)
         nn.init.xavier_normal_(self.fc_ent.weight, gain=gain)
