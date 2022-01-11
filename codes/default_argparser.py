f2e389a1562c6215d2c1a55e4e96bac734f0233d
王广涛 <guangtao.wang@jd.com>
2022-01-11 09:16:10 -0800

    first commit

--- codes/default_argparser.py
+++ codes/default_argparser.py
@@ -99,7 +99,7 @@ def default_parser():
     parser.add_argument('--max_degree', type=int, default=200)
     parser.add_argument('--degree_emb_dim', type=int, default=512)
     # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    parser.add_argument('--gnn_hop_num', type=int, default=6)
+    parser.add_argument('--gnn_hop_num', type=int, default=8)
     parser.add_argument('--alpha', type=float, default=0.1)
     parser.add_argument('--hidden_dim', type=int, default=256)
     parser.add_argument('--head_num', type=int, default=8)
