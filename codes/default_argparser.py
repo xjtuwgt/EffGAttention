dd7994c280e6ab39e7916232b2a91267461a3dd0
王广涛 <guangtao.wang@jd.com>
2022-01-11 20:38:28 -0800

    first commit

--- codes/default_argparser.py
+++ codes/default_argparser.py
@@ -97,7 +97,7 @@ def default_parser():
     parser.add_argument('--stanford', type=boolean_string, default='false')
     # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    parser.add_argument('--gnn_hop_num', type=int, default=8)
+    parser.add_argument('--gnn_hop_num', type=int, default=9)
     parser.add_argument('--alpha', type=float, default=0.1)
     parser.add_argument('--hidden_dim', type=int, default=64)
     parser.add_argument('--head_num', type=int, default=8)
