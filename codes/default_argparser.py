94f1cb32b155ae8ebb75182231e4d025d4aa244e
王广涛 <guangtao.wang@jd.com>
2022-01-11 19:41:28 -0800

    first commit

--- codes/default_argparser.py
+++ codes/default_argparser.py
@@ -91,7 +91,7 @@ def default_parser():
     # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     parser.add_argument('--feat_drop', type=float, default=0.55)
     parser.add_argument('--attn_drop', type=float, default=0.45)
-    parser.add_argument('--edge_drop', type=float, default=0.1)
+    parser.add_argument('--edge_drop', type=float, default=0.05)
     parser.add_argument('--residual', type=boolean_string, default='true')
     parser.add_argument('--ppr_diff', type=boolean_string, default='true')
     parser.add_argument('--stanford', type=boolean_string, default='false')
