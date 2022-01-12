f6c514e68c399f7ffa8f996cadd7ee87b9ee1a0f
王广涛 <guangtao.wang@jd.com>
2022-01-12 10:56:45 -0800

    first commit

--- train_gdt.py
+++ train_gdt.py
@@ -117,8 +117,8 @@ def main(args):
 
     feat_drop_ratio_list = np.arange(0.3, 0.51, 0.05).tolist()
     attn_drop_ratio_list = np.arange(0.3, 0.51, 0.05).tolist()
-    hid_dim_list = [64]
-    layer_num_list = [2]
+    hid_dim_list = [256]
+    layer_num_list = [3]
     edge_drop_ratio_list = [0.0]
     lr_ratio_list = [1e-4, 5e-4, 1e-3, 5e-3]
 
