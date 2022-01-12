631e8212b1db0b078c206ccfa81e76ade20099c2
王广涛 <guangtao.wang@jd.com>
2022-01-12 11:59:48 -0800

    first commit

--- codes/gdt_layers.py
+++ codes/gdt_layers.py
@@ -165,7 +165,7 @@ class GDTLayer(nn.Module):
             for _ in range(self._hop_num):
                 if self._degree_norm and _ > 0:
                     feat = feat * head_norm
-                graph.srcdata['h'] = self.feat_drop(feat)
+                graph.srcdata['h'] = feat
                 graph.edata['a_temp'] = self.attn_drop(attentions)
                 graph.update_all(fn.u_mul_e('h', 'a_temp', 'm'), fn.sum('m', 'h'))
                 feat = graph.dstdata.pop('h')
@@ -322,7 +322,7 @@ class RGDTLayer(nn.Module):
             feat = feat_0.clone()
             attentions = graph.edata.pop('a')
             for _ in range(self._hop_num):
-                graph.srcdata['h'] = self.feat_drop(feat)
+                graph.srcdata['h'] = feat
                 graph.edata['a_temp'] = self.attn_drop(attentions)
                 graph.update_all(fn.u_mul_e('h', 'a_temp', 'm'), fn.sum('m', 'h'))
                 feat = graph.dstdata.pop('h')
