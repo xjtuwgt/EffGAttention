2bdc6c8ec33f62b19a5c042a6e63c9631e898617
王广涛 <guangtao.wang@jd.com>
2022-01-12 12:36:42 -0800

    first commit

--- codes/utils.py
+++ codes/utils.py
@@ -20,3 +20,62 @@ def seed_everything(seed: int) -> int:
     dgl.random.seed(seed)
     dgl.seed(seed)
     return seed
+
+
+def rand_search_parameter(space: dict):
+    para_type = space['type']
+    if para_type == 'fixed':
+        return space['value']
+    if para_type == 'choice':
+        candidates = space['values']
+        value = random.choice(candidates, 1).tolist()[0]
+        return value
+    if para_type == 'range':
+        log_scale = space.get('log_scale', False)
+        low, high = space['bounds']
+        if log_scale:
+            value = random.uniform(low=np.log(low), high=np.log(high), size=1)[0]
+            value = np.exp(value)
+        else:
+            value = random.uniform(low=low, high=high, size=1)[0]
+        return value
+    else:
+        raise ValueError('Training batch mode %s not supported' % para_type)
+
+
+def Citation_HypeParameterSpace():
+    learning_rate = {'name': 'learning_rate', 'type': 'choice', 'values': [1e-4, 2e-4, 5e-4, 1e-3]}
+    attn_drop_ratio = {'name': 'attn_drop_ratio', 'type': 'choice', 'values': [0.25, 0.3, 0.35, 0.4, 0.45, 0.5]}
+    feat_drop_ratio = {'name': 'feat_drop_ratio', 'type': 'choice', 'values': [0.25, 0.3, 0.35, 0.4, 0.45, 0.5]}
+    edge_drop_ratio = {'name': 'edge_drop_ratio', 'type': 'choice', 'values': [0.0, 0.05, 0.1, 0.15]}
+    hop_num = {'name': 'hop_num', 'type': 'choice', 'values': [4, 5, 6, 7, 8, 9, 10]}
+    alpha = {'name': 'alpha', 'type': 'choice', 'values': [0.05, 0.1, 0.15]}
+    hidden_dim = {'name': 'hidden_dim', 'type': 'choice', 'values': [64, 128, 256]}
+    layer_num = {'name': 'layer_num', 'type': 'choice', 'values': [2, 3]}
+    # ++++++++++++++++++++++++++++++++++
+    search_space = [learning_rate, attn_drop_ratio, feat_drop_ratio, edge_drop_ratio, hidden_dim, hop_num, alpha,
+                    layer_num]
+    search_space = dict((x['name'], x) for x in search_space)
+    return search_space
+
+
+def single_task_trial(search_space: dict, rand_seed=42):
+    parameter_dict = {}
+    for key, value in search_space.items():
+        parameter_dict[key] = rand_search_parameter(value)
+    parameter_dict['seed'] = rand_seed
+    return parameter_dict
+
+
+def citation_random_search_hyper_tunner(args, search_space: dict, seed: int):
+    parameter_dict = single_task_trial(search_space=search_space, rand_seed=seed)
+    args.learning_rate = parameter_dict['learning_rate']
+    args.feat_drop = parameter_dict['feat_drop_ratio']
+    args.attn_drop = parameter_dict['attn_drop_ratio']
+    args.edge_drop = parameter_dict['edge_drop_ratio']
+    args.layers = parameter_dict['layer_num']
+    args.gnn_hop_num = parameter_dict['hop_num']
+    args.alpha = parameter_dict['alpha']
+    args.hidden_dim = parameter_dict['hidden_dim']
+    args.seed = parameter_dict['seed']
+    return args, parameter_dict
