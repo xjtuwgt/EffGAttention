ac9c03d4f9863cd3c6c9bc13267caddfc58fccb5
王广涛 <guangtao.wang@jd.com>
2022-01-17 09:10:35 -0800

    first commit

--- codes/gnn_utils.py
+++ codes/gnn_utils.py
@@ -6,6 +6,7 @@ import dgl.function as fn
 from torch import nn
 import torch.nn.functional as F
 from dgl.nn.pytorch.utils import Identity
+from torch.nn import BatchNorm1d
 from torch import Tensor, LongTensor
 
 """
@@ -142,19 +143,42 @@ class PositionWiseFeedForward(nn.Module):
         self.hidden_dim = d_hidden
         self.model_out_dim = model_out_dim
         self.w_1 = nn.Linear(model_dim, d_hidden)
+        self.batch_norm = BatchNorm1d(num_features=d_hidden)
         self.w_2 = nn.Linear(d_hidden, model_out_dim)
         self.dropout = nn.Dropout(dropout)
         self.layer_num = layer_num
         self.init()
 
     def forward(self, x):
-        return self.w_2(self.dropout(F.relu(self.w_1(x))))
+        return self.w_2(self.dropout(F.relu(self.batch_norm(self.w_1(x)))))
 
     def init(self):
         gain = small_init_gain(d_in=self.model_dim, d_out=self.hidden_dim) / math.sqrt(self.layer_num)
         nn.init.xavier_normal_(self.w_1.weight, gain=gain)
         gain = small_init_gain(d_in=self.model_dim, d_out=self.model_out_dim) / math.sqrt(self.layer_num)
         nn.init.xavier_normal_(self.w_2.weight, gain=gain)
+
+
+class LBRLayer(nn.Module):
+    def __init__(self, model_dim, model_out_dim, layer_num=1, dropout: float = None):
+        super(LBRLayer, self).__init__()
+        self.model_dim = model_dim
+        self.model_out_dim = model_out_dim
+        self.batch_norm = BatchNorm1d(num_features=model_out_dim)
+        self.w_1 = nn.Linear(model_dim, model_out_dim)
+        self.layer_num = layer_num
+        if dropout:
+            self.dropout = nn.Dropout(p=dropout)
+        else:
+            self.dropout = Identity()
+        self.init()
+
+    def init(self):
+        gain = small_init_gain(d_in=self.model_dim, d_out=self.model_out_dim) / math.sqrt(self.layer_num)
+        nn.init.xavier_normal_(self.w_1.weight, gain=gain)
+
+    def forward(self, x):
+        return self.dropout(F.relu(self.batch_norm(self.w_1(x))))
 
 
 class MLPReadout(nn.Module):
