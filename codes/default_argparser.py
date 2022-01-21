4c519ae43b3a2d82e9190db83937f28832c1d012
王广涛 <guangtao.wang@jd.com>
2022-01-20 19:39:13 -0800

    first commit

--- codes/default_argparser.py
+++ codes/default_argparser.py
@@ -81,8 +81,9 @@ def default_parser():
                         help='Directory to save model and summaries')
     parser.add_argument('--data_path', type=str, default=HOME_DATA_FOLDER)
     # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    parser.add_argument('--citation_name', type=str, default='citeseer')
-    parser.add_argument('--ogb_node_name', type=str, default='ogbn-arxiv')
+    parser.add_argument('--citation_name', type=str, default='citeseer', choices=['cora', 'citeseer'])
+    parser.add_argument('--ogb_node_name', type=str, default='ogbn-arxiv', choices=['ogbn-products',
+                                                                                    'ogbn-arxiv', 'ogbn-papers100M'])
     parser.add_argument('--graph_type', type=str, default='citation', choices=["citation", "ogb"])
     parser.add_argument('--encoder_v2', type=boolean_string, default='false')
     parser.add_argument('--relation_encoder', type=boolean_string, default='false')
