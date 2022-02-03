6a4da80fee1bd3549da58d6ea5247696cdd0b317
王广涛 <guangtao.wang@jd.com>
2022-02-03 10:10:48 -0800

    first commit

--- graph_data/graph_dataloader.py
+++ graph_data/graph_dataloader.py
@@ -147,14 +147,14 @@ class NodeClassificationSubGraphDataset(Dataset):
     def __init__(self, graph: DGLHeteroGraph, nentity: int, nrelation: int, fanouts: list,
                  special_entity2id: dict, special_relation2id: dict, data_type: str, graph_type: str,
                  bi_directed: bool = True, self_loop: bool = False, edge_dir: str = 'in',
-                 node_split_idx: dict = None, cls: bool = True):
+                 node_split_idx: dict = None, super_cls_node: bool = True):
         assert len(fanouts) > 0 and (data_type in {'train', 'valid', 'test'})
         assert graph_type in {'citation', 'ogb'}
         self.fanouts = fanouts  # list of int == number of hops for sampling
         self.hop_num = len(fanouts)
         assert self.hop_num >= 2
         self.g = graph
-        self.cls = cls
+        self.cls = super_cls_node
         #####################
         if graph_type == 'ogb':
             assert node_split_idx is not None
@@ -177,14 +177,13 @@ class NodeClassificationSubGraphDataset(Dataset):
     def __getitem__(self, idx):
         node_idx = self.data_node_ids[idx]
         anchor_node_ids = torch.LongTensor([node_idx])
-        samp_hop_num = random.randint(2, self.hop_num + 1)  # sample sub-graph from 2-hop to k-hop (k >= 2)
-        samp_fanouts = self.fanouts[:samp_hop_num]
         cls_node_ids = torch.LongTensor([self.special_entity2id['cls']])
+        print(self.fanouts)
         subgraph, parent2sub_dict, _ = \
             anchor_node_sub_graph_extractor(graph=self.g,
                                             anchor_node_ids=anchor_node_ids,
                                             cls_node_ids=cls_node_ids,
-                                            fanouts=samp_fanouts,
+                                            fanouts=self.fanouts,
                                             edge_dir=self.edge_dir,
                                             special_relation2id=self.special_relation2id,
                                             self_loop=self.self_loop,
@@ -212,27 +211,31 @@ class NodeClassificationSubGraphDataset(Dataset):
         return {'batch_graph': (batch_graphs, batch_graph_cls, batch_anchor_id), 'batch_label': batch_label}
 
 
-class NodeClassificationDataHelper(object):
+class NodeClassificationSubGraphDataHelper(object):
     def __init__(self, config):
         self.config = config
         self.graph_type = self.config.graph_type
         if self.graph_type == 'citation':
             graph, number_of_nodes, number_of_relations, n_classes, n_feats, special_node_dict, special_relation_dict = \
                 citation_k_hop_graph_reconstruction(dataset=self.config.citation_node_name,
-                                                    hop_num=self.config.sub_graph_hop_num, rand_split=False,
-                                                    oon=self.config.oon_type, cls=True)
+                                                    hop_num=self.config.sub_graph_hop_num,
+                                                    rand_split=False,
+                                                    oon=self.config.oon_type,
+                                                    cls=True)
             self.node_split_idx = None
         elif self.graph_type == 'ogb':
             graph, number_of_nodes, number_of_relations, n_classes, n_feats, special_node_dict, \
             special_relation_dict, node_split_idx = ogb_k_hop_graph_reconstruction(dataset=self.config.ogb_node_name,
                                                                                    hop_num=self.config.sub_graph_hop_num,
-                                                                                   oon=self.config.oon_type, cls=True)
+                                                                                   oon=self.config.oon_type,
+                                                                                   cls=True)
             self.node_split_idx = node_split_idx
         else:
             raise '{} is not supported'.format(self.graph_type)
 
-        graph = dgl.remove_self_loop(g=graph)
-        graph = graph.int().to(self.config.device)
+        graph = dgl.remove_self_loop(g=graph)  # remove self-loop edge in graph
+        if torch.cuda.is_available():
+            graph = graph.int().to(self.config.device)
         self.graph = graph
         self.number_of_nodes = number_of_nodes
         self.number_of_relations = number_of_relations
@@ -244,20 +247,25 @@ class NodeClassificationDataHelper(object):
         self.train_batch_size = self.config.train_batch_size
         self.val_batch_size = self.config.eval_batch_size
         self.edge_dir = self.config.sub_graph_edge_dir
-        self.self_loop = self.config.sub_graph_self_loop
-        self.fanouts = [int(_) for _ in self.config.sub_graph_fanouts.split(',')]
+        self.self_loop = self.config.sub_graph_self_loop # whether adding self-loop in sub-graph
+        # self.fanouts = [int(_) for _ in self.config.sub_graph_fanouts.split(',')]
+        self.fanouts = [-1 for _ in self.config.sub_graph_fanouts.split(',')]
         self.graph_augmentation = self.config.graph_augmentation
 
     def data_loader(self, data_type):
         assert data_type in {'train', 'valid', 'test'}
-        dataset = NodeClassificationSubGraphDataset(graph=self.graph, nentity=self.number_of_nodes,
+        dataset = NodeClassificationSubGraphDataset(graph=self.graph,
+                                                    nentity=self.number_of_nodes,
                                                     nrelation=self.number_of_relations,
                                                     special_entity2id=self.special_entity_dict,
                                                     special_relation2id=self.special_relation_dict,
-                                                    data_type=data_type, graph_type=self.graph_type,
-                                                    edge_dir=self.edge_dir, self_loop=self.self_loop,
-                                                    fanouts=self.fanouts, cls=True,
-                                                    node_split_idx=self.node_split_idx)
+                                                    data_type=data_type,
+                                                    graph_type=self.graph_type,
+                                                    edge_dir=self.edge_dir,
+                                                    self_loop=self.self_loop,
+                                                    fanouts=self.fanouts,
+                                                    node_split_idx=self.node_split_idx,
+                                                    super_cls_node=True)
         if data_type in {'train'}:
             batch_size = self.train_batch_size
             shuffle = True
@@ -312,14 +320,12 @@ class NodeClassificationSubGraphAugDataset(Dataset):
     def __getitem__(self, idx):
         node_idx = self.data_node_ids[idx]
         anchor_node_ids = torch.LongTensor([node_idx])
-        samp_hop_num = random.randint(2, self.hop_num + 1)
-        samp_fanouts = self.fanouts[:samp_hop_num]
         cls_node_ids = torch.LongTensor([self.special_entity2id['cls']])
         subgraph, parent2sub_dict, _ = \
             anchor_node_sub_graph_extractor(graph=self.g,
                                             anchor_node_ids=anchor_node_ids,
                                             cls_node_ids=cls_node_ids,
-                                            fanouts=samp_fanouts,
+                                            fanouts=self.fanouts,
                                             edge_dir=self.edge_dir,
                                             special_relation2id=self.special_relation2id,
                                             self_loop=self.self_loop,
